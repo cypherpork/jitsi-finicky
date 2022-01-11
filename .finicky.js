57b94f47721a56a829293121445ffc0ad1bd1651
Wohltemperirte Tapir <97419265+cypherpork@users.noreply.github.com>
2022-01-11 10:33:16 -0800

    Minor formatting

--- .finicky.js
+++ .finicky.js
@@ -1,17 +1,19 @@
-module.exports = {
-  defaultBrowser: "Firefox",
-	handlers: [
+    module.exports = 
+    {
+      defaultBrowser: "Firefox",
+      handlers: [
 		{
-            match: ({ url }) => url.host.includes("jitsi.your-selfhosted-server.com") || url.host.includes("meet.jit.si") ,
+                  match: ({ url }) => url.host.includes("jitsi.your-selfhosted-server.com") || 
+                                                                url.host.includes("meet.jit.si") ,
 			      url({ url }) {
 					  return {
 					  		          ...url,
 						              protocol: "jitsi-meet", 
 						              host: url.host, 
 						              pathname: url.pathname
-					  };
-			  },
-			  browser: "/Applications/Jitsi\ Meet.app"
-			  }		
-	]
-};
+					            };
+			                     },
+			       browser: "/Applications/Jitsi\ Meet.app"
+                }		
+	        ]
+    };
